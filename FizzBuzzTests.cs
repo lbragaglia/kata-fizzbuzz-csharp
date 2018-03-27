e71f5799276847bb13caeb4fb2c3562ec25f5856
Lorenzo Bragaglia <lbragaglia@users.noreply.github.com>
2018-03-27 14:07:15 +0200

    Add Bang

--- FizzBuzzTests.cs
+++ FizzBuzzTests.cs
@@ -1,4 +1,3 @@
-using System;
 using Xunit;
 using FluentAssertions;
 using System.Linq;
@@ -21,68 +20,54 @@ namespace kata_fizzbuzz_csharp
         [InlineData(4, "4")]
         [InlineData(5, "Buzz")]
         [InlineData(6, "Fizz")]
-        [InlineData(7, "7")]
+        [InlineData(7, "Bang")]
         [InlineData(8, "8")]
         [InlineData(9, "Fizz")]
         [InlineData(10, "Buzz")]
         [InlineData(11, "11")]
         [InlineData(12, "Fizz")]
         [InlineData(13, "13")]
-        [InlineData(14, "14")]
+        [InlineData(14, "Bang")]
         [InlineData(15, "FizzBuzz")]
+        [InlineData(30, "FizzBuzz")]
+        [InlineData(7*3, "FizzBang")]
+        [InlineData(7*3*2, "FizzBang")]
+        [InlineData(7*5, "BuzzBang")]
+        [InlineData(7*5*2, "BuzzBang")]
+        [InlineData(7*5*3, "FizzBuzzBang")]
+        [InlineData(7*5*3*2, "FizzBuzzBang")]
         public void ShouldPrintInputNumber(int input, string expected)
         {
             _sut.Print(input).Should().Be(expected);
-
         }
     }
 
     internal class FizzBuzzFactory
     {
-        public FizzBuzzFactory()
-        {
-        }
-
         internal FizzBuzzer Create()
         {
-            return new FizzBuzzer(new DefaultTranslator(), new Fizzer(), new Buzzer());
+            return new FizzBuzzer(new DefaultTranslator(), new Fizzer(), new Buzzer(), new Banger());
         }
     }
 
-    internal class Buzzer:INumberTranslator
+    internal class Buzzer: INumberTranslator
     {
-        public Buzzer()
-        {
-        }
-
-        public string Print(int number)
-        {
-            return number % 5 == 0 ? "Buzz" : null;
-        }
+        public string Print(int number) => number % 5 == 0 ? "Buzz" : null;
     }
 
-    internal class DefaultTranslator:INumberTranslator
+    internal class DefaultTranslator: INumberTranslator
     {
-        public DefaultTranslator()
-        {
-        }
-
-        public string Print(int number)
-        {
-            return number.ToString();
-        }
+        public string Print(int number) => number.ToString();
     }
 
     internal class Fizzer: INumberTranslator
     {
-        public Fizzer()
-        {
-        }
+        public string Print(int number) => number % 3 == 0 ? "Fizz" : null;
+    }
 
-        public string Print(int number)
-        {
-            return number % 3 == 0 ? "Fizz" : null;
-        }
+    internal class Banger: INumberTranslator
+    {
+        public string Print(int number) => number % 7 == 0 ? "Bang" : null;
     }
 
     internal interface INumberTranslator
